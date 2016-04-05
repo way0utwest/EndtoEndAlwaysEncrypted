bd603c8b74e5ccdbebc3b3206ea1996cb1f29d02
Steve Jones <steve.jones@red-gate.com>
2016-04-05 16:04:12 -0600

    Misc updates on trip
    Nothing complete, stashing updates.

--- WindowsFormClient/AlwaysEncryptedClient/AlwaysEncryptedClient/Form1.Designer.cs
+++ WindowsFormClient/AlwaysEncryptedClient/AlwaysEncryptedClient/Form1.Designer.cs
@@ -60,18 +60,20 @@
             // 
             // txtInstance
             // 
-            this.txtInstance.Location = new System.Drawing.Point(75, 19);
+            this.txtInstance.Location = new System.Drawing.Point(150, 37);
+            this.txtInstance.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtInstance.Name = "txtInstance";
-            this.txtInstance.Size = new System.Drawing.Size(132, 20);
+            this.txtInstance.Size = new System.Drawing.Size(260, 31);
             this.txtInstance.TabIndex = 0;
             this.txtInstance.Text = ".\\SQL2016";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Location = new System.Drawing.Point(22, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.Size = new System.Drawing.Size(93, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Instance";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -79,52 +81,58 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 54);
+            this.label2.Location = new System.Drawing.Point(22, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(104, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Database";
             // 
             // txtDB
             // 
-            this.txtDB.Location = new System.Drawing.Point(75, 46);
+            this.txtDB.Location = new System.Drawing.Point(150, 88);
+            this.txtDB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtDB.Name = "txtDB";
-            this.txtDB.Size = new System.Drawing.Size(132, 20);
+            this.txtDB.Size = new System.Drawing.Size(260, 31);
             this.txtDB.TabIndex = 3;
             this.txtDB.Text = "AlwaysEncryptedDemo";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 83);
+            this.label3.Location = new System.Drawing.Point(22, 160);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.Size = new System.Drawing.Size(57, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "User";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 112);
+            this.label4.Location = new System.Drawing.Point(22, 215);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.Size = new System.Drawing.Size(106, 25);
             this.label4.TabIndex = 5;
             this.label4.Text = "Password";
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(75, 83);
+            this.txtUser.Location = new System.Drawing.Point(150, 160);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(132, 20);
+            this.txtUser.Size = new System.Drawing.Size(260, 31);
             this.txtUser.TabIndex = 6;
             this.txtUser.Text = "AEUser";
             // 
             // txtPwd
             // 
-            this.txtPwd.Location = new System.Drawing.Point(75, 112);
+            this.txtPwd.Location = new System.Drawing.Point(150, 215);
+            this.txtPwd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtPwd.Name = "txtPwd";
             this.txtPwd.PasswordChar = '*';
-            this.txtPwd.Size = new System.Drawing.Size(132, 20);
+            this.txtPwd.Size = new System.Drawing.Size(260, 31);
             this.txtPwd.TabIndex = 7;
             this.txtPwd.Text = "Blu3Corn";
             // 
@@ -140,9 +148,11 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.txtDB);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(226, 12);
+            this.groupBox1.Location = new System.Drawing.Point(812, 29);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(220, 166);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(440, 319);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Authentication";
@@ -150,9 +160,10 @@
             // chkEncrypt
             // 
             this.chkEncrypt.AutoSize = true;
-            this.chkEncrypt.Location = new System.Drawing.Point(75, 140);
+            this.chkEncrypt.Location = new System.Drawing.Point(150, 269);
+            this.chkEncrypt.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.chkEncrypt.Name = "chkEncrypt";
-            this.chkEncrypt.Size = new System.Drawing.Size(74, 17);
+            this.chkEncrypt.Size = new System.Drawing.Size(141, 29);
             this.chkEncrypt.TabIndex = 10;
             this.chkEncrypt.Text = "Encrypted";
             this.chkEncrypt.UseVisualStyleBackColor = true;
@@ -160,32 +171,36 @@
             // lblConnect
             // 
             this.lblConnect.AutoSize = true;
-            this.lblConnect.Location = new System.Drawing.Point(11, 140);
+            this.lblConnect.Location = new System.Drawing.Point(22, 269);
+            this.lblConnect.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblConnect.Name = "lblConnect";
-            this.lblConnect.Size = new System.Drawing.Size(0, 13);
+            this.lblConnect.Size = new System.Drawing.Size(0, 25);
             this.lblConnect.TabIndex = 9;
             // 
             // txtCustID
             // 
-            this.txtCustID.Location = new System.Drawing.Point(74, 14);
+            this.txtCustID.Location = new System.Drawing.Point(148, 27);
+            this.txtCustID.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtCustID.Name = "txtCustID";
-            this.txtCustID.Size = new System.Drawing.Size(100, 20);
+            this.txtCustID.Size = new System.Drawing.Size(196, 31);
             this.txtCustID.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 17);
+            this.label5.Location = new System.Drawing.Point(12, 33);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.Size = new System.Drawing.Size(124, 25);
             this.label5.TabIndex = 10;
             this.label5.Text = "CustomerID";
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(166, 225);
+            this.btnSearch.Location = new System.Drawing.Point(47, 799);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(119, 23);
+            this.btnSearch.Size = new System.Drawing.Size(238, 44);
             this.btnSearch.TabIndex = 11;
             this.btnSearch.Text = "Get Customers";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -194,16 +209,18 @@
             // dgvCustomers
             // 
             this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCustomers.Location = new System.Drawing.Point(12, 254);
+            this.dgvCustomers.Location = new System.Drawing.Point(24, 488);
+            this.dgvCustomers.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.dgvCustomers.Name = "dgvCustomers";
-            this.dgvCustomers.Size = new System.Drawing.Size(434, 156);
+            this.dgvCustomers.Size = new System.Drawing.Size(1242, 300);
             this.dgvCustomers.TabIndex = 12;
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(166, 416);
+            this.btnQuit.Location = new System.Drawing.Point(1014, 799);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(119, 23);
+            this.btnQuit.Size = new System.Drawing.Size(238, 44);
             this.btnQuit.TabIndex = 13;
             this.btnQuit.Text = "Quit";
             this.btnQuit.UseVisualStyleBackColor = true;
@@ -221,18 +238,21 @@
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.txtCustID);
-            this.groupBox2.Location = new System.Drawing.Point(12, 15);
+            this.groupBox2.Location = new System.Drawing.Point(24, 29);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(197, 204);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox2.Size = new System.Drawing.Size(394, 392);
             this.groupBox2.TabIndex = 14;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Add New Customer";
             // 
             // btnAddCustomer
             // 
-            this.btnAddCustomer.Location = new System.Drawing.Point(32, 160);
+            this.btnAddCustomer.Location = new System.Drawing.Point(70, 324);
+            this.btnAddCustomer.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnAddCustomer.Name = "btnAddCustomer";
-            this.btnAddCustomer.Size = new System.Drawing.Size(122, 29);
+            this.btnAddCustomer.Size = new System.Drawing.Size(244, 56);
             this.btnAddCustomer.TabIndex = 18;
             this.btnAddCustomer.Text = "Add";
             this.btnAddCustomer.UseVisualStyleBackColor = true;
@@ -241,71 +261,79 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(9, 105);
+            this.label8.Location = new System.Drawing.Point(18, 202);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.Size = new System.Drawing.Size(120, 25);
             this.label8.TabIndex = 17;
             this.label8.Text = "Credit Limit";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(7, 78);
+            this.label7.Location = new System.Drawing.Point(14, 150);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.Size = new System.Drawing.Size(110, 25);
             this.label7.TabIndex = 16;
             this.label7.Text = "Authorizer";
             // 
             // chkActive
             // 
             this.chkActive.AutoSize = true;
-            this.chkActive.Location = new System.Drawing.Point(74, 133);
+            this.chkActive.Location = new System.Drawing.Point(148, 256);
+            this.chkActive.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.chkActive.Name = "chkActive";
-            this.chkActive.Size = new System.Drawing.Size(56, 17);
+            this.chkActive.Size = new System.Drawing.Size(103, 29);
             this.chkActive.TabIndex = 15;
             this.chkActive.Text = "Active";
             this.chkActive.UseVisualStyleBackColor = true;
             // 
             // txtCreditLimit
             // 
-            this.txtCreditLimit.Location = new System.Drawing.Point(74, 99);
+            this.txtCreditLimit.Location = new System.Drawing.Point(148, 190);
+            this.txtCreditLimit.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtCreditLimit.Name = "txtCreditLimit";
-            this.txtCreditLimit.Size = new System.Drawing.Size(100, 20);
+            this.txtCreditLimit.Size = new System.Drawing.Size(196, 31);
             this.txtCreditLimit.TabIndex = 14;
             // 
             // txtCreditAuth
             // 
-            this.txtCreditAuth.Location = new System.Drawing.Point(74, 72);
+            this.txtCreditAuth.Location = new System.Drawing.Point(148, 138);
+            this.txtCreditAuth.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtCreditAuth.Name = "txtCreditAuth";
-            this.txtCreditAuth.Size = new System.Drawing.Size(100, 20);
+            this.txtCreditAuth.Size = new System.Drawing.Size(196, 31);
             this.txtCreditAuth.TabIndex = 13;
             // 
             // txtCustomerName
             // 
-            this.txtCustomerName.Location = new System.Drawing.Point(74, 42);
+            this.txtCustomerName.Location = new System.Drawing.Point(148, 81);
+            this.txtCustomerName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtCustomerName.Name = "txtCustomerName";
-            this.txtCustomerName.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerName.Size = new System.Drawing.Size(196, 31);
             this.txtCustomerName.TabIndex = 12;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 43);
+            this.label6.Location = new System.Drawing.Point(12, 83);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(68, 25);
             this.label6.TabIndex = 11;
             this.label6.Text = "Name";
             // 
             // frmAlwaysEncrypted
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 451);
+            this.ClientSize = new System.Drawing.Size(1281, 858);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.dgvCustomers);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "frmAlwaysEncrypted";
             this.Text = "AlwaysEncryptedClient";
             this.Load += new System.EventHandler(this.Form1_Load);
